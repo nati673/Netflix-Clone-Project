0ef8509c1168ae9ff42f3edde3b130646f6910cc
Natnael Hailu <122819655+nati673@users.noreply.github.com>
2023-07-29 13:05:32 -0700

    requests.js
    Remove the api key

--- src/API/requests.js
+++ src/API/requests.js
@@ -1,5 +1,5 @@
 const APIKEY = process.env.REACT_APP_API_KEY
-  //    https://api.themoviedb.org/3/movie/550?api_key=bc7650b2c4d5178002c2ef554604c7c6
+  //    https://api.themoviedb.org/3/movie/550?api_key=*********************************
 const requests = {
   fethTrending: `/trending/all/week?api_key=${APIKEY}&language=en-US`,
   fetchNetflixOriginals: `/discover/tv?api_key=${APIKEY}&with_networks=213`,
